9ca56b0c1d61f36ce0a400604d1dae274f84a927
zhengxhui <1057354161@qq.com>
2023-01-01 22:50:32 +0800

    No.94 二叉树的中序遍历

---
+++ 94.binary-tree-inorder-traversal.js
@@ -0,0 +1,28 @@
+/**
+ * 94. 二叉树的中序遍历
+ * https://leetcode.cn/problems/binary-tree-inorder-traversal/submissions/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  const result = []
+  function traversal(node) {
+    if (!node) {
+      return
+    }
+    traversal(node.left)
+    result.push(node.val)
+    traversal(node.right)
+  }
+
+  traversal(root)
+  return result
+};
